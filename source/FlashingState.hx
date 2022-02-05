753e88402a893f91c169c16d6c02120632b4a826
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-02-05 18:17:31 -0300

    original mod by iccy

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,9 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Original mod is made by Iccy and Galactic Tea, \n
+			This mod is not made by me ok? \n
+			Port by Aly-Ant",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
